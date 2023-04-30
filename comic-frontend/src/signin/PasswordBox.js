136bc34f62ea4e851ebd3b836da931a8e3ac6f38
hetricke <hetricke@umich.edu>
2023-04-29 21:11:24 -0400

    added username and password text fields

---
+++ comic-frontend/src/signin/PasswordBox.js
@@ -0,0 +1,13 @@
+import TextField from '@mui/material/TextField';
+
+function PasswordBox(){
+
+return(
+
+    <TextField id="password_textfield" label="Password" variant="outlined" />
+
+)
+
+}
+
+export default PasswordBox;
---
+++ comic-frontend/src/signin/UsernameBox.js
@@ -0,0 +1,13 @@
+import TextField from '@mui/material/TextField';
+
+function UsernameBox(){
+
+return(
+
+    <TextField id="username_textfield" label="Username" variant="outlined" />
+
+)
+
+}
+
+export default UsernameBox;
